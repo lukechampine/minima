5d98c8facd3a167a505861881ee910b7552b9c4f
lukechampine <luke.champine@gmail.com>
2015-07-02 16:47:54 -0400

    read until parens match

--- repl.go
+++ repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/lukechampine/minima/lang"
 )
@@ -11,23 +12,38 @@ import (
 func main() {
 	fmt.Println("Minima REPL v0.1")
 	r := bufio.NewReader(os.Stdin)
+
+REPL:
 	for {
 		fmt.Print("λ> ")
-		line, err := r.ReadString('\n')
-		if err != nil {
-			fmt.Println("read error:", err)
-			continue
+
+		var expr string
+		// read until parens match
+		var balance int
+		for {
+			line, err := r.ReadString('\n')
+			if err != nil {
+				fmt.Println("read error:", err)
+				continue REPL
+			}
+			balance += strings.Count(line, "(") - strings.Count(line, ")")
+			expr += line
+			if balance == 0 {
+				break
+			}
 		}
-		sexp, err := lang.ReadString(line)
+		sexp, err := lang.ReadString(expr)
 		if err != nil {
 			fmt.Println("parse error:", err)
 			continue
 		}
+
 		sexp, err = lang.Eval(sexp)
 		if err != nil {
 			fmt.Println("error:", err)
 			continue
 		}
+
 		fmt.Println(sexp)
 	}
 }
